76cd7e2b5112a27c00a13d76a4aec9828ced7f22
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-07-24 23:46:33 +0530

    doubly linked list push and pop code

--- datastructures/doubly-linked-list.js
+++ datastructures/doubly-linked-list.js
@@ -12,4 +12,44 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let toRemove = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = toRemove.prev;
+      this.tail.next = null;
+      toRemove.prev = null;
+    }
+
+    this.length--;
+    return toRemove;
+  }
 }
+
+//tests
+
+let list = new DoublyLinkedList();
+
+list.push("parth");
+list.push("patel");
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
