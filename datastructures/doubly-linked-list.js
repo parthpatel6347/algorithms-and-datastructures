8308106b76511b777a8f3019f5ac949a9c3fe52c
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-07-25 16:12:53 +0530

    finish doubly-linked-list

--- datastructures/doubly-linked-list.js
+++ datastructures/doubly-linked-list.js
@@ -42,6 +42,99 @@ class DoublyLinkedList {
     this.length--;
     return toRemove;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let toRemove = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = toRemove.next;
+      this.head.prev = null;
+      toRemove.next = null;
+    }
+
+    this.length--;
+    return toRemove;
+  }
+
+  unShift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    let getNode;
+    if (idx < this.length / 2) {
+      getNode = this.head;
+      for (let i = 0; i < idx; i++) {
+        getNode = getNode.next;
+      }
+    } else {
+      getNode = this.tail;
+      for (let i = this.length - 1; i > idx; i--) {
+        getNode = getNode.prev;
+      }
+    }
+    return getNode;
+  }
+
+  set(val, idx) {
+    let foundNode = this.get(idx);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(val, idx) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) return !!this.unShift(val);
+    if (idx === this.length) return !!this.push(val);
+
+    let newNode = new Node(val);
+    let prevNode = this.get(idx - 1);
+    let nextNode = prevNode.next;
+
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+    nextNode.prev = newNode;
+    newNode.prev = prevNode;
+
+    this.length++;
+    return true;
+  }
+
+  remove(idx) {
+    if (idx < 0 || idx >= this.length) return false;
+    if (idx === 0) return this.shift();
+    if (idx === this.length - 1) return this.pop();
+
+    let toRemove = this.get(idx);
+    let prevNode = toRemove.prev;
+    let nextNode = toRemove.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+
+    toRemove.next = null;
+    toRemove.prev = null;
+
+    this.length--;
+    return toRemove;
+  }
 }
 
 //tests
@@ -50,6 +143,19 @@ let list = new DoublyLinkedList();
 
 list.push("parth");
 list.push("patel");
-console.log(list.pop());
-console.log(list.pop());
-console.log(list);
+list.push(999);
+list.push(888);
+list.push(777);
+list.push(666);
+list.push("second last");
+list.push("last");
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.shift());
+// list.unShift(111);
+// console.log(list.set(9999, 2));
+list.insert("Iron Man", 2);
+console.log(list.remove(2));
+
+console.log(list.get(2));
+// console.log(list);
