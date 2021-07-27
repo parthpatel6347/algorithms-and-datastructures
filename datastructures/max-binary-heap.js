8fdd4259564c701efb7116324c64d168bc2936a6
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-07-27 17:53:03 +0530

    binary head extractMax

--- datastructures/max-binary-heap.js
+++ datastructures/max-binary-heap.js
@@ -33,15 +33,62 @@ class MaxBinaryHeap {
   //     }
   //     return this.values;
   //   }
+
+  extractMax() {
+    swap(this.values, 0, this.values.length - 1);
+    let removed = this.values.pop();
+    let parentIndex = 0;
+    while (true) {
+      let leftChildIdx = parentIndex * 2 + 1;
+      let rightChildIdx = parentIndex * 2 + 2;
+      let greaterChildIdx = null;
+      let leftChild, rightChild;
+      if (leftChildIdx < this.values.length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > this.values[parentIndex]) {
+          greaterChildIdx = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < this.values.length) {
+        rightChild = this.values[rightChildIdx];
+        if (rightChild > leftChild && rightChild > this.values[parentIndex]) {
+          greaterChildIdx = rightChildIdx;
+        }
+      }
+      if (greaterChildIdx === null) break;
+      swap(this.values, greaterChildIdx, parentIndex);
+      parentIndex = greaterChildIdx;
+    }
+    return removed;
+  }
 }
 
 let heap = new MaxBinaryHeap();
 
-console.log(heap.insert(54));
-console.log(heap.insert(48));
-console.log(heap.insert(42));
-console.log(heap.insert(4));
-console.log(heap.insert(20));
-console.log(heap.insert(44));
-console.log(heap.insert(47));
-console.log(heap.insert(95));
+// heap.insert(54);
+// heap.insert(48);
+// heap.insert(42);
+// heap.insert(4);
+// heap.insert(20);
+// heap.insert(44);
+// heap.insert(47);
+// console.log(heap.insert(95));
+// console.log(heap.extractMax());
+// console.log(heap.values)
+
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.values);
